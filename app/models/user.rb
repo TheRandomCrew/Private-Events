90f59651354f19c7de4396b1f24aeb6a00a118d5
Aheebwa Ramadhan <raheebwa@hotmail.com>
2019-11-27 00:43:16 +0300

    [feat] Added users controller - linted with rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   has_secure_password
 
   validates :email, uniqueness: true
   validates :password, length: { minimum: 3 }
+  validates :email, :name, presence: true
 end
