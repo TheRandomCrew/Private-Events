c0c47bde03877a9df52c117b403580dba3ff0555
Aheebwa Ramadhan <raheebwa@hotmail.com>
2019-11-27 19:50:57 +0300

    [fix] Display name in navbar - added bs alerts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[show edit update destroy]
+  before_action :current_user, :set_user, only: %i[show edit update destroy]
 
   # GET /users
   def index
