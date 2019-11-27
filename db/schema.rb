d99790e8cf01c8f002dbf411bf1ba81e4537ea0b
Israel-Laguan <israellaguan@gmail.com>
2019-11-27 12:44:11 -0500

    [feat] Event have a foreign key on user called creator_id
    What?:
    - Add some test to check if association exists
    Breaking Changes?

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_27_171649) do
+ActiveRecord::Schema.define(version: 2019_11_27_173712) do
 
   create_table "events", force: :cascade do |t|
     t.string "date"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2019_11_27_171649) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "creator_id"
+    t.index ["creator_id"], name: "index_events_on_creator_id"
   end
 
   create_table "users", force: :cascade do |t|
