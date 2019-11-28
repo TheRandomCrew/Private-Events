649a657d71719ea5d832d22b797fdbecd2920618
Aheebwa Ramadhan <raheebwa@hotmail.com>
2019-11-28 19:24:10 +0300

    [feat] Added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+users = User.create([
+    {name: 'Aheebwa Ramadhan', email: 'mail@raheebwa.com', password: 'password', password_confirmation: 'password'},
+    {name: 'Israel Laguan', email: 'israel@email.com', password: 'password', password_confirmation: 'password'},
+])
+
+users.each do |user|
+    10.times do
+        @event = user.events.build(location: Faker::Nation.capital_city,
+             date: Faker::Date.between_except(from: 1.year.ago, to: 1.year.from_now, excepted: Date.today),
+                description: Faker::Lorem.paragraph)
+        @event.save
+        
+    end
+end
+
+
