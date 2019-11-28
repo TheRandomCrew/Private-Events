b73425bb13ac4f57b43babd57fc1d100fca14062
Aheebwa Ramadhan <raheebwa@hotmail.com>
2019-11-28 19:23:22 +0300

    [fix] Changed Data Type of date column

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2019_11_27_220755) do
 
   create_table "events", force: :cascade do |t|
-    t.string "date"
+    t.date "date"
     t.string "location"
     t.string "description"
     t.datetime "created_at", null: false
