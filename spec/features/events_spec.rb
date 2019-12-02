4ad317e2d029be00b43eab0750c494cd87ffbf97
Israel-Laguan <israellaguan@gmail.com>
2019-12-02 17:38:08 -0500

    [feat] test: add event creation integration test
    What?:
    
    Breaking Changes?

--- spec/features/events_spec.rb
+++ spec/features/events_spec.rb
@@ -1,6 +1,23 @@
-RSpec.describe 'Hello world', type: :feature do
-    scenario 'Root page' do
-      visit root_path
-      expect(page).to have_content('Private Events')
+RSpec.describe 'Events', type: :feature do
+  describe "You can see all the events", :type => :feature do
+    before :each  do
+      User.create(name: 'Israel Laguan', email: 'israel@email.com', password: 'password', password_confirmation: 'password')
+    end
+    it 'Creates an event' do
+      visit '/sessions/new'
+      find('#signin-email').fill_in('sessions_email', with: 'israel@email.com')
+  
+      within("#signin-password") do
+        fill_in 'sessions_password', with: 'password'
+      end
+      click_button 'Sign in'
+      visit '/events/new'
+      fill_in('Date', with: Time.zone.now)
+      fill_in('Location', with: 'Near you')
+      fill_in('Description', with: 'My fancy event')
+      click_button 'Submit'
+      sleep(5)
+      expect(page).to have_content("successfully created")
     end
   end
+end
