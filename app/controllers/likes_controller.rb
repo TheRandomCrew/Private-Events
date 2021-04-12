843df4496c25e386a96400c7d7586fd083fd9635
S-Toky <stokyr29@gmail.com>
2021-04-12 09:50:33 +0300

    add button like and unlike

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,37 @@
+class LikesController < ApplicationController
+  before_action :find_event
+  before_action :find_like, only: [:destroy]
+
+  def create
+    if already_liked?
+      flash[:notice] = "You can't like more than once"
+    else
+      @event.likes.create(user_id: current_user.id)
+    end
+    redirect_to event_path(@event)
+  end
+
+  def destroy
+    if !(already_liked?)
+      flash[:notice] = "Cannot unlike"
+    else
+      @like.destroy
+    end
+    redirect_to event_path(@event)
+  end
+
+  private
+
+  def find_event
+    @event = Event.find(params[:event_id])
+  end
+
+  def already_liked?
+    Like.where(user_id: current_user.id, event_id:
+    params[:event_id]).exists?
+  end
+
+  def find_like
+    @like = @event.likes.find(params[:id])
+ end
+end
