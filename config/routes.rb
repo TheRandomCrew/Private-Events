12e30b7de1ec9319dd5eed6d45093e1ef921b0ef
S-Toky <stokyr29@gmail.com>
2021-04-19 16:26:22 +0300

    add changed

--- config/routes.rb
+++ config/routes.rb
@@ -17,9 +17,8 @@ Rails.application.routes.draw do
   resources :events do
     resources :comments, only: [:create, :destroy]
   end
-
   resources :comments do
-    resources :answers
+    resources :answers, only: [:create, :destroy]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
